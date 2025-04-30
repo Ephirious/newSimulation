4218400f681eee90eabe30b023d0a1bf21a226a3
Ephirious <Ephirious@gmail.com>
2025-04-30 20:46:05 +0300

    release: first release

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
+import commands.Command;
+import commands.factories.InitCommandsFactory;
+import commands.factories.TurnCommandsFactory;
+import path_finder.AStar;
+import path_finder.PathFinder;
+import renderer.Renderer;
+import renderer.SimulationRenderer;
+import simulation.Simulation;
+import simulation.SimulationInformation;
+import simulation_map.MediumMapFactory;
+import simulation_map.SimulationMap;
+import util.sprites.SpriteFactory;
+import util.sprites.Sprites;
+
+import java.util.List;
+
+
 public class Main {
+    public static void main(String[] args) {
+        SimulationMap worldMap = new MediumMapFactory().createMap();
+        SimulationInformation information = new SimulationInformation(worldMap);
+        Sprites sprites = SpriteFactory.getDefaultSprites();
+        Renderer renderer = new SimulationRenderer(worldMap, sprites, information);
+
+        PathFinder finder = new AStar(worldMap);
+        List<Command> initCommands = new InitCommandsFactory(worldMap, finder).getCommands();
+        List<Command> turnCommand = new TurnCommandsFactory(worldMap).getCommands();
+
+        Simulation simulation = new Simulation(worldMap, renderer, information, initCommands, turnCommand);
+        simulation.startSimulation();
+    }
 }
