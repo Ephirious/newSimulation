5d8d730201f572d3d4802480eed1dd14de642aa0
Ephirious <Ephirious@gmail.com>
2025-04-30 18:42:22 +0300

    feat(SimulationRenderer): render world map, info and buttons now

--- src/renderer/SimulationRenderer.java
+++ src/renderer/SimulationRenderer.java
@@ -2,20 +2,42 @@ package renderer;
 
 import coordinates.Coordinates;
 import entities.Entity;
-import simulationmap.SimulationMap;
+import key_reader.KeyReader;
+import simulation.SimulationInformation;
+import simulation_map.SimulationMap;
 import util.sprites.Sprites;
 
 public class SimulationRenderer implements Renderer {
     private final SimulationMap worldMap;
     private final Sprites sprites;
+    private final SimulationInformation information;
 
-    public SimulationRenderer(SimulationMap worldMap, Sprites sprites) {
+    public SimulationRenderer(SimulationMap worldMap, Sprites sprites, SimulationInformation information) {
         this.worldMap = worldMap;
         this.sprites = sprites;
+        this.information = information;
     }
 
     public void render() {
+        renderInfo();
+        renderButtons();
         renderWorldMap();
+    }
+
+    private void renderInfo() {
+        System.out.println("Number of turn: " + information.getCountTurns());
+        System.out.println("Number of grasses: " + information.getCountGrass());
+        System.out.println("Number of herbivores: " + information.getCountHerbivores());
+        System.out.println("Number of predators: " + information.getCountPredators());
+    }
+
+    private void renderButtons() {
+        System.out.printf("%c - pause; %c - continue; %c - restart; %c - quit\n",
+                KeyReader.KEY_PAUSE,
+                KeyReader.KEY_CONTINUE,
+                KeyReader.KEY_RESTART,
+                KeyReader.KEY_QUIT
+        );
     }
 
     private void renderWorldMap() {
