01e540fe8726b6e05ccd7701e21230fa21fdd51d
Ephirious <Ephirious@gmail.com>
2025-04-30 11:46:47 +0300

    refactor(AbstractMapFactory): add lamda for `PlaceEntityCommand`

--- src/simulationmap/AbstractMapFactory.java
+++ src/simulationmap/AbstractMapFactory.java
@@ -9,6 +9,7 @@ import entities.unmoved.UnmovedEntitiesFactory;
 import entities.unmoved.UnmovedEntityType;
 import util.enums.Damage;
 import util.enums.Speed;
+import util.map.SimulationMapUtils;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -33,33 +34,38 @@ public abstract class AbstractMapFactory {
         int countPredators = ((int) (mapSize * PREDATOR_PERCENT));
 
 
-
         initCommands.add(new PlaceEntityCommand(
                 worldMap,
+                () -> SimulationMapUtils.getFreeCoordinates(worldMap),
                 countRocks,
                 new UnmovedEntitiesFactory(UnmovedEntityType.ROCK)
         ));
         initCommands.add(new PlaceEntityCommand(
                 worldMap,
+                () -> SimulationMapUtils.getFreeCoordinates(worldMap),
                 countTrees,
                 new UnmovedEntitiesFactory(UnmovedEntityType.TREE)
         ));
         initCommands.add(new PlaceEntityCommand(
                 worldMap,
+                () -> SimulationMapUtils.getFreeCoordinates(worldMap),
                 countGrasses,
                 new UnmovedEntitiesFactory(UnmovedEntityType.GRASS)
         ));
 
         initCommands.add(new PlaceEntityCommand(
                 worldMap,
+                () -> SimulationMapUtils.getFreeCoordinates(worldMap),
                 countHerbivores,
                 new HerbivoreFactory(Creature.MAX_HP, Speed.getRandomSpeed())
         ));
         initCommands.add(new PlaceEntityCommand(
                 worldMap,
+                () -> SimulationMapUtils.getFreeCoordinates(worldMap),
                 countPredators,
                 new PredatorFactory(Creature.MAX_HP, Speed.getRandomSpeed(), Damage.getRandomDamage())
         ));
+
 
         for (Command current : initCommands) {
             current.execute();
